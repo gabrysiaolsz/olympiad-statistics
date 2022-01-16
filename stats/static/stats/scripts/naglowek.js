b6056243dd4c587f42716d4ae6967bb7ded74d9d
Gabrysia <gabriela.olszewska11@gmail.com>
2022-01-16 18:59:46 +0100

    fix js script

--- stats/static/stats/scripts/naglowek.js
+++ stats/static/stats/scripts/naglowek.js
@@ -1,35 +1,35 @@
-var number = Math.floor(Math.random()*4)+1;
+let number = 0;
+let timer1 = 0;
+let timer2 = 0;
 
-var timer1 = 0;
-var timer2 = 0;
-
-function runaway()
-{
+function runaway() {
     $("#nag").fadeOut(500);
 }
 
-function change()
-{
-    number++; 
+function change() {
+    number = (number + 1) % 5;
 
-    if (number>5)
-    {
-	number=1;	
+    let image = document.getElementById("nag");
+
+    switch (number) {
+        case 0:
+            image.src="/static/stats/img/nag/naglowek1.jpg";
+            break;
+        case 1:
+            image.src="/static/stats/img/nag/naglowek2.jpg";
+            break;
+        case 2:
+            image.src="/static/stats/img/nag/naglowek3.jpg";
+            break;
+        case 3:
+            image.src="/static/stats/img/nag/naglowek4.jpg";
+            break;
+        case 4:
+            image.src="/static/stats/img/nag/naglowek5.jpg";
+            break;
     }
+    $("#nag").fadeIn(500);
 
-    switch(number)
-    {
-
-        case number:
-        {
-            $('#nag').html("<img src=\"img/nag/naglowek" + number + ".jpg\" class=\"slajd-naglowek\" />");
-        }
-        break;
-
-    }
-    $('#nag').fadeIn(500);
-
-    timer1 = setTimeout("change()", 5000); 
+    timer1 = setTimeout("change()", 5000);
     timer2 = setTimeout("runaway()", 4500);
-
 }
